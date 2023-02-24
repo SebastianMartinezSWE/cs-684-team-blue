3c93fc41c8ddc7335188cd549df876fe1f630024
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-02-23 21:04:16 -0500

    Added MongoDB connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,19 @@
-import express from "express";
-const app = express();
-const port = 5000;
+import "dotenv/config";
+import mongoose from "mongoose";
+import env from "../src/util/validateEnv";
+import app from "./app";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const port = env.PORT;
 
-app.listen(port, () => {
-  console.log("Server is running on port: " + port);
-});
+// Preparing for Mongoose 7 since strictQuery will be deprecated
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(port, () => {
+      console.log("Server is running on Port: " + port);
+    });
+  })
+  .catch(console.error);
