6ec18c2d799509c0448ce7d52fba81ef8e124bdc
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-02-23 21:05:23 -0500

    Controller to get all users, user and user by id

---
+++ backend/src/controllers/users.ts
@@ -0,0 +1,68 @@
+import { RequestHandler } from "express";
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
+import UserModel from "../models/user";
+
+export const getUsers: RequestHandler = async (req, res, next) => {
+  try {
+    const users = await UserModel.find().exec();
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUser: RequestHandler = async (req, res, next) => {
+  const userId = req.params.userId;
+  try {
+    if (!mongoose.isValidObjectId(userId)) {
+      throw createHttpError(400, "Invalid user id");
+    }
+
+    const user = await UserModel.findById(userId).exec();
+
+    if (!user) {
+      throw createHttpError(404, "User not found");
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+interface CreateUserBody {
+  firstName?: string;
+  username?: string;
+  password?: string;
+}
+
+export const createUser: RequestHandler<
+  unknown,
+  unknown,
+  CreateUserBody,
+  unknown
+> = async (req, res, next) => {
+  const firstName = req.body.firstName;
+  const username = req.body.username;
+  const password = req.body.password;
+
+  try {
+    if (!firstName || !username || !password) {
+      throw createHttpError(
+        400,
+        "User should have include the First Name, Username and Password"
+      );
+    }
+
+    const newUser = await UserModel.create({
+      firstName: firstName,
+      username: username,
+      password: password,
+    });
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    next(error);
+  }
+};
