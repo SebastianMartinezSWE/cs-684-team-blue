fa2a38e2a583b21fbc1a92ad243ca197758f06a4
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-02-24 18:51:43 -0500

    Created first sprint home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,57 @@
-import logo from "./logo.svg";
-import styles from "./styles/App.module.css";
+import { useEffect, useState } from "react";
+import * as UserApi from "./api/user";
+import SignInModal from "./components/modals/SignInModal";
+import SignUpModal from "./components/modals/SignUpModal";
+import DefaultNavbar from "./components/navbar/DefaultNavbar";
+import SignedInView from "./components/views/SignedInView";
+import SignedOutView from "./components/views/SignedOutView";
+import { User } from "./models/user";
 
 function App() {
+  const [signedInUser, setSignedInUser] = useState<User | null>(null);
+
+  const [showSignUpModal, setShowSignUpModal] = useState(false);
+  const [showSignInModal, setShowSignInModal] = useState(false);
+
+  useEffect(() => {
+    async function fetchSignedInUser() {
+      try {
+        const user = await UserApi.getSignedInUser();
+        setSignedInUser(user);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchSignedInUser();
+  }, []);
+
   return (
-    <div className={styles.App}>
-      <header className={styles.AppHeader}>
-        <img src={logo} className={styles.AppLogo} alt="logo" />
-      </header>
+    <div>
+      <DefaultNavbar
+        signedInUser={signedInUser}
+        onSignInClicked={() => setShowSignInModal(true)}
+        onSignUpClicked={() => setShowSignUpModal(true)}
+        onSignOutSuccessful={() => setSignedInUser(null)}
+      />
+      <>{signedInUser ? <SignedInView /> : <SignedOutView />}</>
+      {showSignUpModal && (
+        <SignUpModal
+          onDismiss={() => setShowSignUpModal(false)}
+          onSignUpSuccessful={(user) => {
+            setSignedInUser(user);
+            setShowSignUpModal(false);
+          }}
+        />
+      )}
+      {showSignInModal && (
+        <SignInModal
+          onDismiss={() => setShowSignInModal(false)}
+          onSigninSuccessful={(user) => {
+            setSignedInUser(user);
+            setShowSignInModal(false);
+          }}
+        />
+      )}
     </div>
   );
 }
