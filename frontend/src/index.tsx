f5486fbc66d14bddc9d1a6b5f6633cfdceb11704
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-02-24 14:44:11 -0500

    Added CSS modules

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,8 +2,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+import "./styles/global.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
