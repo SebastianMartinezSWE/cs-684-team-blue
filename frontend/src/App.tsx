760754186dd05042d1840756afc903fec59ccd37
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-02-25 15:35:10 -0500

    Added home and page not found routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import * as UserApi from "./api/user";
 import SignInModal from "./components/modals/SignInModal";
 import SignUpModal from "./components/modals/SignUpModal";
 import DefaultNavBar from "./components/navbar/DefaultNavBar";
-import SignedInView from "./components/views/SignedInView";
-import SignedOutView from "./components/views/SignedOutView";
 import { User } from "./models/user";
+import HomePage from "./pages/HomePage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   const [signedInUser, setSignedInUser] = useState<User | null>(null);
@@ -26,33 +27,38 @@ function App() {
   }, []);
 
   return (
-    <div>
-      <DefaultNavBar
-        signedInUser={signedInUser}
-        onSignInClicked={() => setShowSignInModal(true)}
-        onSignUpClicked={() => setShowSignUpModal(true)}
-        onSignOutSuccessful={() => setSignedInUser(null)}
-      />
-      <>{signedInUser ? <SignedInView /> : <SignedOutView />}</>
-      {showSignUpModal && (
-        <SignUpModal
-          onDismiss={() => setShowSignUpModal(false)}
-          onSignUpSuccessful={(user) => {
-            setSignedInUser(user);
-            setShowSignUpModal(false);
-          }}
+    <BrowserRouter>
+      <div>
+        <DefaultNavBar
+          signedInUser={signedInUser}
+          onSignInClicked={() => setShowSignInModal(true)}
+          onSignUpClicked={() => setShowSignUpModal(true)}
+          onSignOutSuccessful={() => setSignedInUser(null)}
         />
-      )}
-      {showSignInModal && (
-        <SignInModal
-          onDismiss={() => setShowSignInModal(false)}
-          onSigninSuccessful={(user) => {
-            setSignedInUser(user);
-            setShowSignInModal(false);
-          }}
-        />
-      )}
-    </div>
+        <Routes>
+          <Route path="/" element={<HomePage signedInUser={signedInUser} />} />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
+        {showSignUpModal && (
+          <SignUpModal
+            onDismiss={() => setShowSignUpModal(false)}
+            onSignUpSuccessful={(user) => {
+              setSignedInUser(user);
+              setShowSignUpModal(false);
+            }}
+          />
+        )}
+        {showSignInModal && (
+          <SignInModal
+            onDismiss={() => setShowSignInModal(false)}
+            onSigninSuccessful={(user) => {
+              setSignedInUser(user);
+              setShowSignInModal(false);
+            }}
+          />
+        )}
+      </div>
+    </BrowserRouter>
   );
 }
 
