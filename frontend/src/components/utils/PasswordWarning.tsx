e552a729df3f2290329ab0750ee95c542127e9e7
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-02-25 16:18:50 -0500

    Requirements for Username and Password

---
+++ frontend/src/components/utils/PasswordWarning.tsx
@@ -0,0 +1,13 @@
+const PasswordWarning = () => {
+  return (
+    <ul>
+      <li>Password must be at least 8 characters long</li>
+      <li>
+        Must include at least 1 uppercase letter and at least 1 lowercase letter
+      </li>
+      <li>Must include at least 1 special character (!@#$%)</li>
+    </ul>
+  );
+};
+
+export default PasswordWarning;
---
+++ frontend/src/components/utils/UserNameWarning.tsx
@@ -0,0 +1,9 @@
+const UserNameWarning = () => {
+  return (
+    <ul>
+      <li>Username must be at least 8 characters long</li>
+    </ul>
+  );
+};
+
+export default UserNameWarning;
