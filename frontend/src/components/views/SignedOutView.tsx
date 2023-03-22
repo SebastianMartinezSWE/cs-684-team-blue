9b62b5690650e3c4b3a910ab1949ea99fcd9b3c3
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-03-21 20:09:59 -0400

    Added General News articles to display

--- frontend/src/components/views/SignedOutView.tsx
+++ frontend/src/components/views/SignedOutView.tsx
@@ -1,13 +1,44 @@
-import logo from "../../logo.svg";
-import styles from "../../styles/App.module.css";
+import { useEffect, useState } from "react";
+import { Button, Col, Row } from "react-bootstrap";
+import { getNews } from "../../api/news";
+import { News } from "../../models/news";
+import styles from "../../styles/NewsPage.module.css";
+import styleUtil from "../../styles/utils/util.module.css";
+import Article from "../cards/Article";
 
 const SignedOutView = () => {
+  const [articles, setArticles] = useState<News["articles"]>([]);
+
+  useEffect(() => {
+    async function loadArticles() {
+      try {
+        const news = await getNews("default");
+        setArticles(news.articles);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+    loadArticles();
+  }, []);
+
   return (
     <>
-      <header className={styles.AppHeader}>
-        <img src={logo} className={styles.AppLogoSignedOut} alt="logo" />
-        <p>Hi! Please Sign in or Create an account</p>
-      </header>
+      <h1
+        className={`display-1 mt-4 mb-2 text-center text-white font-weight-bold`}
+      >
+        General News
+      </h1>
+      <Button className={`mb-4 ${styleUtil.centerItem}`} onClick={() => {}}>
+        Refresh
+      </Button>
+      <Row xs={1} md={2} xl={3} className={`g-4`}>
+        {articles?.map((article) => (
+          <Col key={article.title}>
+            <Article className={styles.article} article={article} />
+          </Col>
+        ))}
+      </Row>
     </>
   );
 };
