97c0bb214203d2c1166bd0c07ef683a951857e0b
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-03-23 21:02:26 -0400

    Mirgrating to  Vite + React + TS + SWC

--- frontend/src/api/user.ts
+++ frontend/src/api/user.ts
@@ -30,7 +30,9 @@ async function fetchData(input: RequestInfo, init?: RequestInit) {
 }
 
 export async function getSignedInUser(): Promise<User> {
-  const response = await fetchData("/api/users", { method: "GET" });
+  const response = await fetchData("http://localhost:8080/api/users", {
+    method: "GET",
+  });
   return response.json();
 }
 
