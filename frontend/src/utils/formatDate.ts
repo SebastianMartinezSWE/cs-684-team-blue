e6c58a34bd29b8053b77dd6e2821ae4f2c03514c
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-03-27 15:52:11 -0400

    Not needed file

--- frontend/src/utils/formatDate.ts
+++
@@ -1,7 +0,0 @@
-export function formatDate(dateString: Date): string {
-  return new Date(dateString).toLocaleDateString("en-US", {
-    year: "numeric",
-    month: "short",
-    day: "numeric",
-  });
-}
