d7b02ebe9e5cbefbf8ad25373d768921f98a4c28
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-03-27 12:38:39 -0400

    Added functionality for settings

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,15 +1,21 @@
 import { InferSchemaType, model, Schema } from "mongoose";
+import { settingsSchema } from "./settings";
 
 const userSchema = new Schema({
   username: {
     type: String,
     minLength: 8,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
     minLength: 8,
     required: true,
+  },
+  settings: {
+    type: settingsSchema,
+    default: () => ({}),
   },
 });
 
