75e70ea4d29fbf8eb27fb19f9b8f4ec0434e90ac
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-03-27 12:10:44 -0400

    Added settings api

---
+++ frontend/src/api/settings.ts
@@ -0,0 +1,57 @@
+import { User } from "../models/user";
+import {
+  BadRequestError,
+  ConflictError,
+  UnauthorizedError,
+} from "../utils/HttpErrors";
+
+async function fetchData(input: RequestInfo, init?: RequestInit) {
+  const response = await fetch(input, init);
+  if (response.ok) {
+    return response;
+  } else {
+    const errorBody = await response.json();
+    const errorMessage = errorBody.error;
+    if (response.status === 400) {
+      throw new BadRequestError(errorMessage);
+    } else if (response.status === 401) {
+      throw new UnauthorizedError(errorMessage);
+    } else if (response.status === 409) {
+      throw new ConflictError(errorMessage);
+    } else {
+      throw Error(
+        "Request failed with status: " +
+          response.status +
+          ", message: " +
+          errorMessage
+      );
+    }
+  }
+}
+
+export interface Settings {
+  general: boolean;
+  business: boolean;
+  entertainment: boolean;
+  health: boolean;
+  science: boolean;
+  sports: boolean;
+  technology: boolean;
+}
+
+export async function updateSettings(
+  userId: string,
+  settings: Settings
+): Promise<User> {
+  const response = await fetchData(
+    `http://localhost:8080/api/settings/${userId}`,
+    {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(settings),
+    }
+  );
+  return response.json();
+}
