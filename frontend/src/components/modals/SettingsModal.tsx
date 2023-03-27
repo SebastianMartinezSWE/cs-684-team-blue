d6cf7933c0c6c7e2fd89df13c272a965d980675b
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-03-27 12:10:53 -0400

    Added settings modal

---
+++ frontend/src/components/modals/SettingsModal.tsx
@@ -0,0 +1,124 @@
+import { useState } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import * as SettingsApi from "../../api/settings";
+import { User } from "../../models/user";
+interface SettingsModalProps {
+  onDismiss: () => void;
+  userData: User;
+  showSettingsModal: boolean;
+}
+
+const SettingsModal = ({
+  onDismiss,
+  userData,
+  showSettingsModal,
+}: SettingsModalProps) => {
+  const [settingsData, setSettingsData] = useState<User["settings"]>(
+    userData.settings
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const value = e.target.checked;
+    setSettingsData((prevValues) => {
+      return { ...prevValues, [name]: value };
+    });
+  };
+
+  async function saveSettings(updatedSettings: User["settings"]) {
+    try {
+      await SettingsApi.updateSettings(userData._id, updatedSettings);
+      userData.settings = updatedSettings;
+    } catch (error) {
+      console.error(error);
+      alert(error);
+    }
+  }
+
+  return (
+    <Modal
+      show={showSettingsModal}
+      onHide={() => {
+        onDismiss();
+        setSettingsData(userData.settings);
+      }}
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title data-testid="Settings-Modal">Settings</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form.Label>Choose your news preferences:</Form.Label>
+        <Form id="settings">
+          <Form.Check
+            type="checkbox"
+            id="general-checkbox"
+            label="General"
+            name="general"
+            checked={settingsData.general}
+            onChange={handleChange}
+          />
+          <Form.Check
+            label="Business"
+            checked={settingsData.business}
+            name="business"
+            onChange={handleChange}
+          />
+          <Form.Check
+            label="Entertainment"
+            checked={settingsData.entertainment}
+            name="entertainment"
+            onChange={handleChange}
+          />
+          <Form.Check
+            label="Health"
+            checked={settingsData.health}
+            name="health"
+            onChange={handleChange}
+          />
+          <Form.Check
+            label="Science"
+            checked={settingsData.science}
+            name="science"
+            onChange={handleChange}
+          />
+          <Form.Check
+            label="Sports"
+            checked={settingsData.sports}
+            name="sports"
+            onChange={handleChange}
+          />
+          <Form.Check
+            label="Technology"
+            checked={settingsData.technology}
+            name="technology"
+            onChange={handleChange}
+          />
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          variant="secondary"
+          onClick={() => {
+            setSettingsData(userData.settings);
+            onDismiss();
+          }}
+        >
+          Close
+        </Button>
+        <Button
+          type="submit"
+          variant="primary"
+          onClick={() => {
+            saveSettings(settingsData);
+            onDismiss();
+          }}
+        >
+          Save Changes
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default SettingsModal;
