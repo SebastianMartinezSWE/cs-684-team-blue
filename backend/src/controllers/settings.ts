286f4ca1b7834d51d1349f920b39ba6eb6378149
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-03-29 18:16:47 -0400

    Added Prettier

--- backend/src/controllers/settings.ts
+++ backend/src/controllers/settings.ts
@@ -1,68 +1,68 @@
-import { RequestHandler } from "express";
-import createHttpError from "http-errors";
-import mongoose from "mongoose";
-import UserModel from "../models/user";
-import { assertIsDefined } from "../util/assertIsDefined";
+import { RequestHandler } from 'express'
+import createHttpError from 'http-errors'
+import mongoose from 'mongoose'
+import UserModel from '../models/user'
+import { assertIsDefined } from '../util/assertIsDefined'
 
 interface UpdateSettingsParams {
-  userId: string;
+    userId: string
 }
 
 interface UpdateSettingsBody {
-  general: boolean;
-  business: boolean;
-  entertainment: boolean;
-  health: boolean;
-  science: boolean;
-  sports: boolean;
-  technology: boolean;
+    general: boolean
+    business: boolean
+    entertainment: boolean
+    health: boolean
+    science: boolean
+    sports: boolean
+    technology: boolean
 }
 
 export const updateSettings: RequestHandler<
-  UpdateSettingsParams,
-  unknown,
-  UpdateSettingsBody,
-  unknown
+    UpdateSettingsParams,
+    unknown,
+    UpdateSettingsBody,
+    unknown
 > = async (req, res, next) => {
-  const authenticatedUserId = req.session.userId;
+    const authenticatedUserId = req.session.userId
 
-  const userId = req.params.userId;
-  const general = req.body.general;
-  const business = req.body.business;
-  const entertainment = req.body.entertainment;
-  const health = req.body.health;
-  const science = req.body.science;
-  const sports = req.body.sports;
-  const technology = req.body.technology;
-  try {
-    assertIsDefined(authenticatedUserId);
+    const userId = req.params.userId
+    const general = req.body.general
+    const business = req.body.business
+    const entertainment = req.body.entertainment
+    const health = req.body.health
+    const science = req.body.science
+    const sports = req.body.sports
+    const technology = req.body.technology
+    try {
+        assertIsDefined(authenticatedUserId)
 
-    if (!mongoose.isValidObjectId(userId)) {
-      throw createHttpError(400, "Invalid User ID");
+        if (!mongoose.isValidObjectId(userId)) {
+            throw createHttpError(400, 'Invalid User ID')
+        }
+
+        const user = await UserModel.findById(userId).exec()
+
+        if (!user) {
+            throw createHttpError(404, 'User not found')
+        }
+
+        if (!user._id.equals(authenticatedUserId)) {
+            throw createHttpError(401, 'You cannot modify these settings')
+        }
+
+        user.settings.general = general
+        user.settings.business = business
+        user.settings.entertainment = entertainment
+        user.settings.health = health
+        user.settings.science = science
+        user.settings.sports = sports
+        user.settings.technology = technology
+
+        const updatedSettings = await user.save()
+
+        res.status(200).json(updatedSettings)
+    } catch (error) {
+        next(error)
     }
-
-    const user = await UserModel.findById(userId).exec();
-
-    if (!user) {
-      throw createHttpError(404, "User not found");
-    }
-
-    if (!user._id.equals(authenticatedUserId)) {
-      throw createHttpError(401, "You cannot modify these settings");
-    }
-
-    user.settings.general = general;
-    user.settings.business = business;
-    user.settings.entertainment = entertainment;
-    user.settings.health = health;
-    user.settings.science = science;
-    user.settings.sports = sports;
-    user.settings.technology = technology;
-
-    const updatedSettings = await user.save();
-
-    res.status(200).json(updatedSettings);
-  } catch (error) {
-    next(error);
-  }
-};
+}
