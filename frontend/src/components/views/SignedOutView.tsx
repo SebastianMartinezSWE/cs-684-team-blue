b5f8afe55c5d587425ba4fdde630c2eca24888e9
Sebastian Martinez <sebastianalexmartinez87@gmail.com>
2023-04-06 11:36:16 -0400

    Added tabs and changed button location

--- frontend/src/components/views/SignedOutView.tsx
+++ frontend/src/components/views/SignedOutView.tsx
@@ -1,45 +1,106 @@
 import { useEffect, useState } from "react";
 import { Button, ButtonGroup, Col, Row, Tab, Tabs } from "react-bootstrap";
 import { ArrowClockwise } from "react-bootstrap-icons";
+import { getCategory } from "../../api/category";
 import { getDefaultNews } from "../../api/news";
 import { News } from "../../models/news";
 import Article from "../cards/Article";
 
 const SignedOutView = () => {
-    const [articles, setArticles] = useState<News["articles"]>([]);
+    const [generalArticles, setGeneralArticles] = useState<News["articles"]>(
+        []
+    );
+    const [businessArticles, setBusinessArticles] = useState<News["articles"]>(
+        []
+    );
+    const [entertainmentArticles, setEntertainmentArticles] = useState<
+        News["articles"]
+    >([]);
+
+    const [healthArticles, setHealthArticles] = useState<News["articles"]>([]);
+    const [scienceArticles, setScienceArticles] = useState<News["articles"]>(
+        []
+    );
+    const [sportsArticles, setSportsArticles] = useState<News["articles"]>([]);
+    const [technologyArticles, setTechnologyArticles] = useState<
+        News["articles"]
+    >([]);
+
+    async function loadBusinessArticles() {
+        try {
+            const news = await getCategory("business");
+            setBusinessArticles(news.articles);
+        } catch (error) {
+            console.error(error);
+            alert(error);
+        }
+    }
+
+    async function loadEntertainmentArticles() {
+        try {
+            const news = await getCategory("business");
+            setEntertainmentArticles(news.articles);
+        } catch (error) {
+            console.error(error);
+            alert(error);
+        }
+    }
+
+    async function loadGeneralArticles() {
+        try {
+            const news = await getDefaultNews();
+            setGeneralArticles(news.articles);
+        } catch (error) {
+            console.error(error);
+            alert(error);
+        }
+    }
+
+    async function loadHealthArticles() {
+        try {
+            const news = await getCategory("business");
+            setHealthArticles(news.articles);
+        } catch (error) {
+            console.error(error);
+            alert(error);
+        }
+    }
+    async function loadScienceArticles() {
+        try {
+            const news = await getCategory("business");
+            setScienceArticles(news.articles);
+        } catch (error) {
+            console.error(error);
+            alert(error);
+        }
+    }
+    async function loadSportsArticles() {
+        try {
+            const news = await getCategory("business");
+            setSportsArticles(news.articles);
+        } catch (error) {
+            console.error(error);
+            alert(error);
+        }
+    }
+    async function loadTechnologyArticles() {
+        try {
+            const news = await getCategory("business");
+            setTechnologyArticles(news.articles);
+        } catch (error) {
+            console.error(error);
+            alert(error);
+        }
+    }
 
     useEffect(() => {
-        async function loadArticles() {
-            try {
-                const news = await getDefaultNews();
-                setArticles(news.articles);
-            } catch (error) {
-                console.error(error);
-                alert(error);
-            }
-        }
-        loadArticles();
+        loadGeneralArticles();
+        loadBusinessArticles();
     }, []);
 
     return (
         <>
-            <Tabs
-                defaultActiveKey="home"
-                id="category-tabs"
-                fill
-                className="mb-3"
-            >
-                <Tab eventKey="home" title="Home"></Tab>
-                <Tab eventKey="business" title="Business"></Tab>
-                <Tab eventKey="entertainment" title="Entertainment"></Tab>
-                <Tab eventKey="general" title="General"></Tab>
-                <Tab eventKey="health" title="Health"></Tab>
-                <Tab eventKey="science" title="Science"></Tab>
-                <Tab eventKey="sports" title="Sports"></Tab>
-                <Tab eventKey="technology" title="Technology"></Tab>
-            </Tabs>
-
-            <Row className="d-flex flex-row-reverse bd-highlight mt-1 mb-2">
+            <Row className="d-flex flex-row-reverse bd-highlight mb-3">
                 <Col xs="auto">
                     <ButtonGroup aria-label="Refresh-Settings">
                         <Button
@@ -53,14 +114,85 @@ const SignedOutView = () => {
                     </ButtonGroup>
                 </Col>
             </Row>
-
-            <Row xs={1} sm={2} xl={3} className={`g-4`}>
-                {articles.map((article) => (
-                    <Col key={article.url}>
-                        <Article article={article} />
-                    </Col>
-                ))}
-            </Row>
+            <Tabs
+                defaultActiveKey="home"
+                id="category-tabs"
+                className="mb-3"
+                justify
+            >
+                <Tab eventKey="home" title="Home">
+                    <Row xs={1} sm={2} xl={3} className={`g-4`}>
+                        {generalArticles?.map((articles) => (
+                            <Col key={articles.url}>
+                                <Article article={articles} />
+                            </Col>
+                        ))}
+                    </Row>
+                </Tab>
+                <Tab eventKey="business" title="Business">
+                    <Row xs={1} sm={2} xl={3} className={`g-4`}>
+                        {businessArticles?.map((article) => (
+                            <Col key={article.url}>
+                                <Article article={article} />
+                            </Col>
+                        ))}
+                    </Row>
+                </Tab>
+                <Tab eventKey="entertainment" title="Entertainment">
+                    <Row xs={1} sm={2} xl={3} className={`g-4`}>
+                        {entertainmentArticles?.map((articles) => (
+                            <Col key={articles.url}>
+                                <Article article={articles} />
+                            </Col>
+                        ))}
+                    </Row>
+                </Tab>
+                <Tab eventKey="general" title="General">
+                    <Row xs={1} sm={2} xl={3} className={`g-4`}>
+                        {generalArticles?.map((articles) => (
+                            <Col key={articles.url}>
+                                <Article article={articles} />
+                            </Col>
+                        ))}
+                    </Row>
+                </Tab>
+                <Tab eventKey="health" title="Health">
+                    <Row xs={1} sm={2} xl={3} className={`g-4`}>
+                        {healthArticles?.map((articles) => (
+                            <Col key={articles.url}>
+                                <Article article={articles} />
+                            </Col>
+                        ))}
+                    </Row>
+                </Tab>
+                <Tab eventKey="science" title="Science">
+                    <Row xs={1} sm={2} xl={3} className={`g-4`}>
+                        {scienceArticles?.map((articles) => (
+                            <Col key={articles.url}>
+                                <Article article={articles} />
+                            </Col>
+                        ))}
+                    </Row>
+                </Tab>
+                <Tab eventKey="sports" title="Sports">
+                    <Row xs={1} sm={2} xl={3} className={`g-4`}>
+                        {sportsArticles?.map((articles) => (
+                            <Col key={articles.url}>
+                                <Article article={articles} />
+                            </Col>
+                        ))}
+                    </Row>
+                </Tab>
+                <Tab eventKey="technology" title="Technology">
+                    <Row xs={1} sm={2} xl={3} className={`g-4`}>
+                        {technologyArticles?.map((articles) => (
+                            <Col key={articles.url}>
+                                <Article article={articles} />
+                            </Col>
+                        ))}
+                    </Row>
+                </Tab>
+            </Tabs>
         </>
     );
 };
